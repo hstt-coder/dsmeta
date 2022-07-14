22da45bc7cc7e139ba68bb00d91d6bf69905b4ad
hstt-coder <eduardolopes19@outlook.com>
2022-07-13 22:01:36 -0300

    Twilio SMS

--- backend/src/main/java/com/hsttcoder/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/hsttcoder/dsmeta/controllers/SaleController.java
@@ -2,15 +2,11 @@ package com.hsttcoder.dsmeta.controllers;
 
 import com.hsttcoder.dsmeta.entities.Sale;
 import com.hsttcoder.dsmeta.services.SalesService;
+import com.hsttcoder.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,12 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class SaleController {
     @Autowired
     private SalesService service;
-
+    @Autowired
+    private SmsService smsService;
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value="minDate", defaultValue = "") String minDate,
             @RequestParam(value="maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
